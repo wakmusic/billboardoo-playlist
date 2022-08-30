e42d4a1da44e4d3d5be5f6727db7046ca95f60ac
songiwon <biob1121@naver.com>
2022-08-30 10:32:30 +0900

    feat: SharingPlayListModal 컴포넌트 세팅

--- billboardoo/src/App.js
+++ billboardoo/src/App.js
@@ -9,9 +9,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <PlayListPage />
+      <LoginModal />
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ billboardoo/src/components/SharingPlayListModal/SharingListModal.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SharingListModal = () => {};
+
+export default SharingListModal
