35dc2d4dd072725af27e6acba05f974c17621a22
songiwon <biob1121@naver.com>
2022-10-18 20:29:43 +0900

    refactor: 날자 . 붙이기, 숫자 3자릿수 마다 , 붙이기 알고리즘 구현

--- billboardoo/src/components/PlusMusicPage/MusicBox.jsx
+++ billboardoo/src/components/PlusMusicPage/MusicBox.jsx
@@ -3,12 +3,6 @@ import MusicPlus from "../../assets/svgs/MusicPlus.svg";
 import * as S from "./styled";
 
 const MusicBox = ({ item }) => {
-  const [date, setDate] = useState("20" + item.date.toString());
-
-  useEffect(() => {
-    setDate(date.replace(/(\d{4})(\d{2})(\d{2})/, "$1.$2.$3"));
-  }, []);
-
   return (
     <S.MusicBox>
       <S.MusicImageBack />
@@ -18,10 +12,13 @@ const MusicBox = ({ item }) => {
         <S.MusicArtist>{item.artist}</S.MusicArtist>
       </S.MusicTextLayout>
       <S.MusicInfoText color="#202F61" right="180px">
-        {date}
+        {("20" + item.date.toString()).replace(
+          /(\d{4})(\d{2})(\d{2})/,
+          "$1.$2.$3"
+        )}
       </S.MusicInfoText>
       <S.MusicInfoText color="#202F61" right="90px">
-        {item.views}
+        {item.views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
       </S.MusicInfoText>
       <S.MusicPlusButton>
         <img src={MusicPlus} />
